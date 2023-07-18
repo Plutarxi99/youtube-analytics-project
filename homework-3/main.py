402231bed7a0668c0df0daa38c199073001981df
Egor Shievanov <shievanov@bk.ru>
2023-07-18 20:52:51 +0300

    Реализовал магические методы сложения, вычитания и сравненмя по количеству подписчиков

--- homework-3/main.py
+++ homework-3/main.py
@@ -1,9 +1,11 @@
-from src.channel import Channel
+from channel_later_delete_3 import Channel
 
 if __name__ == '__main__':
     # Создаем два экземпляра класса
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
     highload = Channel('UCwHL6WHUarjGfUM_586me8w')
+    moscowpython.get_attribut()
+    highload.get_attribut()
 
     # Используем различные магические методы
     print(moscowpython)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
